8c102b71a0bd45bf82206ae32b78ac2999aa6645
esharmagenective <esha.sharma@genective.com>
2023-05-04 09:26:02 -0500

    Coded plot 4 and created png file

--- Plot 4.R
+++ Plot 4.R
@@ -1,2 +1,22 @@
 #Across the United States, 
 #how have emissions from coal combustion-related sources changed from 1999–2008?
+
+coal<-subset(SCC, SCC.Level.Three=="Bituminous/Subbituminous Coal")
+coalscc<-coal$SCC
+coalscc<-as.numeric(coalscc)
+
+library(dplyr)
+NEIcoal<-filter(NEI,NEI$SCC %in% coalscc)
+
+years<-split(NEIcoal,NEIcoal$year)
+sums<-data.frame()
+for (i in years) {
+    x<-sum(i$Emissions)
+    sums<-rbind(sums,x)
+}
+colnames(sums)<-c("Sum")
+sums<-cbind(sums,Year=c(1999,2002,2005,2008))
+
+png("plot4.png")
+barplot(sums$Sum, names.arg = sums$Year, xlab = "Year", ylab = "Total PM2.5 Emission by Coal (tons)", main = "Total PM2.5 Emission by Coal", col = "darkblue")
+dev.off()
